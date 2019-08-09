8a5057b441560fdd9fadee2a1347faa9690fb3bd
Peter Jourgensen <48814862+pjourgensen@users.noreply.github.com>
2019-08-09 15:23:01 -0700

    Create main.py

---
+++ Scripts/main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+  import load_data
+  import load_target
+  import init_lists
+  import train_test_split
+  import NN_iter
+  import gen_wts
+  import gen_preds
+  import acc_plot
+  import gen_scores
+  import knn_accs
